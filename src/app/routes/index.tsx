ea0ff41b2d1be7524ba3555c357291bdb9c4a18f
João Neto <joaonetoneto1331@gmail.com>
2024-10-17 17:07:27 -0300

    feat: add dashboard page and react router dom

---
+++ src/app/routes/index.tsx
@@ -0,0 +1,18 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes as Switch,
+  Navigate,
+} from "react-router-dom";
+import { Dashboard } from "../pages";
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/pagina-inicial" Component={Dashboard} />
+        <Route path="*" Component={() => <Navigate to="/pagina-inicial" />} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
