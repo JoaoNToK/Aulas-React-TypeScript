c19de7ee852033b871a8554f1e738641fe58df97
João Neto <joaonetoneto1331@gmail.com>
2024-10-19 22:18:16 -0300

    feat: API de context

---
+++ src/app/shared/contexts/UsuarioLogado.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createContext } from "react";
+
+interface IUsuarioLogadoContextData {
+  nomeDoUsuario: string;
+}
+
+const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData>(
+  {} as IUsuarioLogadoContextData
+);
+
+export const UsuarioLogadoProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: "João" }}>
+      {children}
+    </UsuarioLogadoContext.Provider>
+  );
+};
